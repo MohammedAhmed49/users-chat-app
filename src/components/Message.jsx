7059a71e88a6d04c7a8a49818db04d1373d44f33
m.ahmed <m.ahmed@easternenterprise.com>
2022-10-06 18:28:27 +0200

    Finished UI of message component

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img src="https://images.pexels.com/photos/12547195/pexels-photo-12547195.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+      </div>
+    </div>
+  );
 };
 
 export default Message;
